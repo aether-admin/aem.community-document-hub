0cb3968fc3071a6ab065ffa447bf388d8c199814
Gia Bao (Billy) Tran <btran@solutiondigital.biz>
2023-02-20 11:05:17 +0700

    Invite link on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -22,13 +22,34 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={siteConfig.title}
-      description='A place to document and share learning materials to all developers whom might be working for Adobe Experience Manager'>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <>
+      <Layout
+        title={siteConfig.title}
+        description='A place to document and share learning materials to all developers whom might be working for Adobe Experience Manager'>
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </Layout>
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => {
+          useEffect( () => {
+            const CMS = require('netlify-cms-app');
+            const uploadcare = require('netlify-cms-media-library-uploadcare');
+
+            CMS.registerMediaLibrary(uploadcare)
+            CMS.init();
+
+            const netlifyIdentity = require('netlify-identity-widget');
+            netlifyIdentity.init({
+              container: '#netlify-modal', // defaults to document.body
+              locale: 'en' // defaults to 'en'
+            });
+          }, [])
+
+          return <div id='netlify-modal' />;
+        }}
+      </BrowserOnly>
+      </>
   );
 }
